1fc48629c4dfa1447fe96883da6b2c5e6123e749
vaghasiyakalpesh <148939090+vaghasiyakalpesh@users.noreply.github.com>
2025-06-08 16:13:31 +0530

    Add portfolio section with carousel functionality and update service/resume sections

--- js/script.js
+++ js/script.js
@@ -15,3 +15,49 @@ resumeBtns.forEach((btn, index) => {
     resumeDetail[index].classList.add("active");
   });
 });
+
+const arrowRigth = document.querySelector(
+  ".portfolio-box .naviation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .naviation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlides = document.querySelector(".portfolio-box .img-slide");
+
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlides.style.transform = `translate(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+};
+
+arrowRigth.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRigth.classList.add("disabled");
+  }
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRigth.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+  activePortfolio();
+});
